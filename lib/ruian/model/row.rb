c640e05a8c1f7aa4f7c75decf93b58322e0c3d12
Jindrich Skupa <jindrich.skupa@gmail.com>
2017-12-08 07:32:25 +0100

    Fix RUIAN full import, input line to struct

--- lib/ruian/model/row.rb
+++ lib/ruian/model/row.rb
@@ -1,8 +1,8 @@
 class Ruian
   class Model
-    class Row < Struct.new(:adm_code, :city_code, :city_name,
-                           :momc_name, :mop_name, :city_part_code,
-                           :city_part_name, :street_name, :so_type,
+    class Row < Struct.new(:adm_code, :city_code, :city_name, :momc_code,
+                           :momc_name, :mop_code, :mop_name, :city_part_code,
+                           :city_part_name, :street_name_code, :street_name, :so_type,
                            :e, :o, :o_code, :zip, :gis_y, :gis_x, :valid_from)
     end
   end
